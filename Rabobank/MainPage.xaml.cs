4239cf79f4deb5e4ed4321800405dbca91ba7f89
Pieter van Zutven <pietervanzutven@outlook.com>
2021-12-30 20:57:39 +0100

    Activate back button

--- Rabobank/MainPage.xaml.cs
+++ Rabobank/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -27,12 +29,24 @@ namespace Rabobank
             this.InitializeComponent();
 
             webview.Navigate(new Uri("https://bankieren.rabobank.nl/online/"));
+
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            SystemNavigationManager.GetForCurrentView().BackRequested += MainPage_BackRequested;
         }
 
         private void Webview_NewWindowRequested(WebView sender, WebViewNewWindowRequestedEventArgs args)
         {
             args.Handled = true;
             webview.Navigate(args.Uri);
+        }
+
+        private void MainPage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (webview.CanGoBack)
+            {
+                e.Handled = true;
+                webview.GoBack();
+            }
         }
     }
 }
